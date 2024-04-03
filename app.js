8f875b0e5ffc688bb5d10d02406667958d38376f
amyph <amypholley@gmail.com>
2024-04-03 15:39:32 -0500

    Code Typed

--- app.js
+++ app.js
@@ -7,13 +7,45 @@ console.log(
 console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
+let a = 20;
+let b = 4;
+let sum = a + b;
+let minus = a - b;
+let multiple = a * b;
+let divide = a / b;
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
+let num = ll;
+let str = "11";
+let str2 = "eleven";
+let isPresent= true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+num + str
+num + str2
+num + isPresent
+firstName + num
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
+
+val == str3
+val === str3
+!isAwake
+("eleven" == str4 && val >= str3)
+(!isAwake || isAwake)
+0 == false
+0 === false
+0 != false
+0 !== false
+
